6e320329fcabe2c98bc0b436e0f18e71c4ab6b37
patapelada <77882237+patapelada@users.noreply.github.com>
2025-06-16 22:49:10 +0200

    fix: apply changed cell api

--- src/types.ts
+++ src/types.ts
@@ -226,28 +226,29 @@ export const Turns: { [key: string]: TurnType } = {
 } as const;
 
 const cellId = {
-  A1: "A1",
-  A2: "A2",
-  A3: "A3",
-  A4: "A4",
-  B1: "B1",
-  B2: "B2",
-  B3: "B3",
-  B4: "B4",
-  C1: "C1",
-  C2: "C2",
-  C3: "C3",
-  C4: "C4",
-  D1: "D1",
-  D2: "D2",
-  D3: "D3",
-  D4: "D4",
+  A1: 0b1100,
+  A2: 0b1000,
+  A3: 0b0100,
+  A4: 0b0000,
+  B1: 0b1101,
+  B2: 0b1001,
+  B3: 0b0101,
+  B4: 0b0001,
+  C1: 0b1110,
+  C2: 0b1010,
+  C3: 0b0110,
+  C4: 0b0010,
+  D1: 0b1111,
+  D2: 0b1011,
+  D3: 0b0111,
+  D4: 0b0011,
 } as const;
 export const CellIdSchema = z.nativeEnum(cellId);
 export type CellId = z.infer<typeof CellIdSchema>;
 
 export const CellSchema = z.object({
   id: CellIdSchema,
+  name: z.string(),
   position: z.object({
     row: z.number(),
     col: z.number(),
@@ -258,66 +259,82 @@ export type CellType = z.infer<typeof CellSchema>;
 export const Cells: { [key: string]: CellType } = {
   [CellIdSchema.enum.A1]: {
     id: CellIdSchema.enum.A1,
+    name: "A1",
     position: { row: 3, col: 0 },
   },
   [CellIdSchema.enum.A2]: {
     id: CellIdSchema.enum.A2,
+    name: "A2",
     position: { row: 2, col: 0 },
   },
   [CellIdSchema.enum.A3]: {
     id: CellIdSchema.enum.A3,
+    name: "A3",
     position: { row: 1, col: 0 },
   },
   [CellIdSchema.enum.A4]: {
     id: CellIdSchema.enum.A4,
+    name: "A4",
     position: { row: 0, col: 0 },
   },
   [CellIdSchema.enum.B1]: {
     id: CellIdSchema.enum.B1,
+    name: "B1",
     position: { row: 3, col: 1 },
   },
   [CellIdSchema.enum.B2]: {
     id: CellIdSchema.enum.B2,
+    name: "B2",
     position: { row: 2, col: 1 },
   },
   [CellIdSchema.enum.B3]: {
     id: CellIdSchema.enum.B3,
+    name: "B3",
     position: { row: 1, col: 1 },
   },
   [CellIdSchema.enum.B4]: {
     id: CellIdSchema.enum.B4,
+    name: "B4",
     position: { row: 0, col: 1 },
   },
   [CellIdSchema.enum.C1]: {
     id: CellIdSchema.enum.C1,
+    name: "C1",
     position: { row: 3, col: 2 },
   },
   [CellIdSchema.enum.C2]: {
     id: CellIdSchema.enum.C2,
+    name: "C2",
     position: { row: 2, col: 2 },
   },
   [CellIdSchema.enum.C3]: {
     id: CellIdSchema.enum.C3,
+    name: "C3",
     position: { row: 1, col: 2 },
   },
   [CellIdSchema.enum.C4]: {
     id: CellIdSchema.enum.C4,
+    name: "C4",
     position: { row: 0, col: 2 },
   },
   [CellIdSchema.enum.D1]: {
     id: CellIdSchema.enum.D1,
+    name: "D1",
     position: { row: 3, col: 3 },
   },
   [CellIdSchema.enum.D2]: {
     id: CellIdSchema.enum.D2,
+    name: "D2",
     position: { row: 2, col: 3 },
   },
   [CellIdSchema.enum.D3]: {
     id: CellIdSchema.enum.D3,
+    name: "D3",
     position: { row: 1, col: 3 },
   },
   [CellIdSchema.enum.D4]: {
     id: CellIdSchema.enum.D4,
+    name: "D4",
     position: { row: 0, col: 3 },
   },
 } as const;
